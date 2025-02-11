e3ecaefb70ec32a708521986c4b83dbebaffb5e8
OHESTSIS <roussos.ado@gmail.com>
2025-02-11 13:31:59 +0200

    Sword Update
    I made the sword animations and prefabs, added them in the skill manager but I am facing some errors with the sword animations planning to fix them.

--- RPG GAME/Assets/Scripts/Skills/SkillManager.cs
+++ RPG GAME/Assets/Scripts/Skills/SkillManager.cs
@@ -9,6 +9,7 @@ public class SkillManager : MonoBehaviour
 
     public DashSkill dash {get; private set;}
     public CloneSkill clone {get; private set;}
+    public SwordSkill sword {get; private set;}
 
     private void Awake()
     {
@@ -23,5 +24,6 @@ public class SkillManager : MonoBehaviour
     {
         dash = GetComponent<DashSkill>();
         clone = GetComponent<CloneSkill>();
+        sword = GetComponent<SwordSkill>();
     }
 }
