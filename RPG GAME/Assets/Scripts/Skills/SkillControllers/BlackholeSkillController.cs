06528c654cb726e4c58e823e0388e101bc5c15f7
OHESTSIS <roussos.ado@gmail.com>
2025-02-23 20:26:37 +0200

    Blackhole Update
    I updated the blackhole's functionality and now the enemies do not freeze again when they are outside of the blackhole radius.

--- RPG GAME/Assets/Scripts/Skills/SkillControllers/BlackholeSkillController.cs
+++ RPG GAME/Assets/Scripts/Skills/SkillControllers/BlackholeSkillController.cs
@@ -7,34 +7,67 @@ public class BlackholeSkillController : MonoBehaviour
     [SerializeField] private GameObject hotKeyPrefab;
     [SerializeField] private List<KeyCode> KeyCodeList;
 
-    public float maxSize;
-    public float growSpeed;
-    public float shrinkSpeed;
-    public bool canGrow;
-    public bool canShrink;
+    private float maxSize;
+    private float growSpeed;
+    private float shrinkSpeed;
 
-
+    private bool canGrow = true;
+    private bool canShrink;
     private bool canCreateHotKeys = true;
     private bool cloneAttackReleased;
-    public int amountOfAttacks = 4;
-    public float cloneAttackCooldown = .3f;
+
+
+    private int amountOfAttacks = 4;
+    private float cloneAttackCooldown = .3f;
     private float cloneAttackTimer;
 
     private List<Transform> targets = new List<Transform>();
     private List<GameObject> createdHotKey = new List<GameObject>();
+
+    public void SetupBlackhole(float _maxSize,float _growSpeed,float _shrinkSpeed,int _amountOfAttacks,float _cloneAttackCooldown)
+    {
+        maxSize = _maxSize;
+        growSpeed = _growSpeed;
+        shrinkSpeed = _shrinkSpeed;
+        amountOfAttacks = _amountOfAttacks;
+        cloneAttackCooldown = _cloneAttackCooldown;
+    }
 
     private void Update()
     {
         cloneAttackTimer -= Time.deltaTime;
 
 
-        if(Input.GetKeyDown(KeyCode.R))
+        if (Input.GetKeyDown(KeyCode.R))
         {
-            DestroyHotKeys();
-            cloneAttackReleased = true;
-            canCreateHotKeys = false;
+            ReleaseCloneAttack();
         }
 
+        CloneAttackLogic();
+
+        if (canGrow && !canShrink)
+        {
+            transform.localScale = Vector2.Lerp(transform.localScale, new Vector2(maxSize, maxSize), growSpeed * Time.deltaTime);
+        }
+
+        if (canShrink)
+        {
+            transform.localScale = Vector2.Lerp(transform.localScale, new Vector2(-1, -1), shrinkSpeed * Time.deltaTime);
+
+            if (transform.localScale.x < 0)
+                Destroy(gameObject);
+        }
+    }
+
+    private void ReleaseCloneAttack()
+    {
+        DestroyHotKeys();
+        cloneAttackReleased = true;
+        canCreateHotKeys = false;
+    }
+
+    private void CloneAttackLogic()
+    {
         if (cloneAttackTimer < 0 && cloneAttackReleased)
         {
             cloneAttackTimer = cloneAttackCooldown;
@@ -48,30 +81,14 @@ public class BlackholeSkillController : MonoBehaviour
             else
                 xOffset = -2;
 
-            SkillManager.instance.clone.CreateClone(targets[randomIndex], new Vector3(xOffset,0));
+            SkillManager.instance.clone.CreateClone(targets[randomIndex], new Vector3(xOffset, 0));
             amountOfAttacks--;
 
-            if(amountOfAttacks <= 0)
+            if (amountOfAttacks <= 0)
             {
                 canShrink = true;
                 cloneAttackReleased = false;
             }
-        }
-
-
-
-
-        if (canGrow && !canShrink)
-        {
-            transform.localScale = Vector2.Lerp(transform.localScale, new Vector2(maxSize, maxSize), growSpeed * Time.deltaTime);
-        }
-
-        if(canShrink)
-        {
-            transform.localScale = Vector2.Lerp(transform.localScale, new Vector2(-1, -1), shrinkSpeed * Time.deltaTime);
-
-            if (transform.localScale.x < 0)
-                Destroy(gameObject);
         }
     }
 
@@ -93,6 +110,12 @@ public class BlackholeSkillController : MonoBehaviour
             collision.GetComponent<Enemy>().FreezeTime(true);
             CreateHotKey(collision);
         }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.GetComponent<Enemy>() != null)
+            collision.GetComponent<Enemy>().FreezeTime(false);
     }
 
     private void CreateHotKey(Collider2D collision)
