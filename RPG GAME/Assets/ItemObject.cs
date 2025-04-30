3ca809946c45a2e220e78299afbd86e9b316d133
OHESTSIS <roussos.ado@gmail.com>
2025-04-30 14:18:36 +0300

    Inventory-UI/ Items Creation
    Today I created a working inventory for the player, added some Items and he can stack them also. Made several scripts , icons and inventory slots for the player to have and stack items that he can pick when killing the enemies.

---
+++ RPG GAME/Assets/ItemObject.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class ItemObject : MonoBehaviour
+{
+    [SerializeField] private ItemData itemData;
+
+    private void OnValidate()
+    {
+        GetComponent<SpriteRenderer>().sprite = itemData.icon;
+        gameObject.name = "Item object - " + itemData.itemName;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Player>() != null)
+        {
+            Inventory.instance.AddItem(itemData);
+            Destroy(gameObject);
+        }
+    }
+}
