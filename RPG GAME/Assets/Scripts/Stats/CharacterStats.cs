33fb2c701abee5e4ab77a0f7d94114025499e721
OHESTSIS <roussos.ado@gmail.com>
2025-05-11 16:05:53 +0300

    Player drop System
    I made a system that when the player dies he will have a percentage of chance to lose his items and drop them.

--- RPG GAME/Assets/Scripts/Stats/CharacterStats.cs
+++ RPG GAME/Assets/Scripts/Stats/CharacterStats.cs
@@ -49,7 +49,7 @@ public class CharacterStats : MonoBehaviour
 
 
     public System.Action onHealthChanged;
-    protected bool isDead;
+    public bool isDead {get; private set;}
 
 
     protected virtual void Start()
