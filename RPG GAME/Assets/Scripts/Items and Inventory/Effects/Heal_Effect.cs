3303b47449a171c89e64e9593f93fd9e8be301d9
OHESTSIS <roussos.ado@gmail.com>
2025-05-14 13:56:55 +0300

    Heal Item Effects
    Today I created and added a healing flask which heals 30% of your current health and has a cooldown of 5 seconds. Also Added a dracula's sword which have life steal when you hit the enemies.

---
+++ RPG GAME/Assets/Scripts/Items and Inventory/Effects/Heal_Effect.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Heal effect", menuName = "Data/Item effect/Heal effect")]
+public class Heal_Effect : ItemEffect
+{
+
+    [Range(0f,1f)]
+    [SerializeField] private float healPercent;
+
+    public override void ExecuteEffect(Transform _enemyPosition)
+    {
+
+        PlayerStats playerStats = PlayerManager.instance.player.GetComponent<PlayerStats>();
+
+        int healAmount = Mathf.RoundToInt(playerStats.GetMaxHealthValue() * healPercent);
+
+        playerStats.IncreaseHealthBy(healAmount);
+    }
+}
--- RPG GAME/Assets/Scripts/Items and Inventory/Inventory.cs
+++ RPG GAME/Assets/Scripts/Items and Inventory/Inventory.cs
@@ -26,6 +26,10 @@ public class Inventory : MonoBehaviour
     private UI_ItemSlot[] inventoryitemSlot;
     private UI_ItemSlot[] stashItemSlot;
     private UI_EquipmentSlot[] equipmentSlot;
+
+    [Header("Items cooldown")]
+    private float lastTimeUsedFlask;
+
     private void Awake()
     {
         if (instance == null)
@@ -251,5 +255,23 @@ public class Inventory : MonoBehaviour
 
         return equipedItem;
 
+    }
+
+    public void UseFlask()
+    {
+        ItemData_Equipment currentFlask = GetEquipment(EquipmentType.Flask);
+
+        if (currentFlask == null)
+            return;
+
+        bool canUseFlask = Time.time > lastTimeUsedFlask + currentFlask.itemCooldown;
+
+        if (canUseFlask)
+        {
+            currentFlask.Effect(null);
+            lastTimeUsedFlask = Time.time;
+        }
+        else
+            Debug.Log("Flask on Cooldown;");
     }
 }
